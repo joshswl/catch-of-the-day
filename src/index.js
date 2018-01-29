fde0a246a85a66fdbeb41a0d81b9fe8511e330a5
Josh Lee <joshswl@gmail.com>
2018-01-28 21:55:43 -0600

    render

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>hello</p>
+    }
+}
+
+render(<StorePicker/>, document.querySelector('#main'));
