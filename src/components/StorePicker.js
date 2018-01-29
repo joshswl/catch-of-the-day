1cd813bec5e1f5418e9b6ab1daf836542a703f25
Josh Lee <joshswl@gmail.com>
2018-01-29 16:13:22 -0600

    how to comment in React JSX

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,6 +4,7 @@ class StorePicker extends React.Component {
     render() {
         return (
             <form className="store-selector">
+                { /* Hello */ }
                 <h2>Please Enter A Store</h2>
                 <input type="text" required placeholder="Store Name" />
                 <button type="submit">Visit Store -></button>
