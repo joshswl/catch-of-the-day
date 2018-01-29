41d3a5c3f230ad70f2b5fff7a3726cda1c68b004
Josh Lee <joshswl@gmail.com>
2018-01-28 22:01:28 -0600

    refactored component with proper filing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import StorePicker from './components/StorePicker.js';
 
-class StorePicker extends React.Component {
-    render() {
-        return <p>hello</p>
-    }
-}
 
 render(<StorePicker/>, document.querySelector('#main'));
