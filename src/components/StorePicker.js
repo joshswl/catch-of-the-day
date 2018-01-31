de61125cdaa36093c795f7662aaf60f560ab13ec
Josh Lee <joshswl@gmail.com>
2018-01-30 22:48:21 -0600

    goToStore onSubmit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the URL');
+        // first grab the text from the box
+        // second transition from / to /store/:storeId
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 { /* this is how to comment */ }
                 <h2>Please Enter A Store</h2>
                 <input type="text" required placeholder="Store Name"
