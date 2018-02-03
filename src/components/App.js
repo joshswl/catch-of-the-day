31c5a12238a7a26a20046add0946356fb3b639ae
Josh Lee <joshswl@gmail.com>
2018-02-03 17:58:55 -0600

    each sample has a unique Key + Props (details)

--- src/components/App.js
+++ src/components/App.js
@@ -42,8 +42,9 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="list-of-fishes">
                         {
-                            Object.keys(this.state.fishes)
-                                .map(key => <Fish />)
+                            Object
+                                .keys(this.state.fishes)
+                                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
                         }
                     </ul>
                 </div>
