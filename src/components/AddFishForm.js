86b27b26d7e47fd4e7223f453893483419c60589
Josh Lee <joshswl@gmail.com>
2018-02-02 23:03:32 -0600

    createFish onSubmit

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+    createFish(event) {
+        event.preventDefault();
+        console.log('time to make some fish! 🐟')
+    }
     render() {
         return (
-            <form className="fish-edit">
+            <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
                 <input type="text" placeholder="Fish Name" />
                 <input type="text" placeholder="Fish Price" />
                 <select>
