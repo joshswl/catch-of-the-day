f57a1f0493560eb6fdef0f8f911fc34024ff97b7
Josh Lee <joshswl@gmail.com>
2018-02-03 18:00:57 -0600

    each sample has a unique name displayed HTML

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Fish extends React.Component {
     render() {
         return (
-            <li className="menu-fish">🐟</li>
+            <li className="menu-fish">
+                {this.props.details.name}
+            </li>
         )
     }
 }
