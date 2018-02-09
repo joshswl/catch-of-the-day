a963765189a964958ce786ec7d0ebd475f558c4d
Josh Lee <joshswl@gmail.com>
2018-02-08 20:43:29 -0600

    base

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDN4lJ26pzpdqwtHBbP--qVQEAhmWVPtHw",
+    authDomain: "catch-of-the-day-josh-lee.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-josh-lee.firebaseio.com",
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
     constructor() {
