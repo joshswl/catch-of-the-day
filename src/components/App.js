6556e9daf603529f98f0f9906820e02f643486b8
Josh Lee <joshswl@gmail.com>
2018-02-14 21:09:32 -0600

    removeFish()

--- src/components/App.js
+++ src/components/App.js
@@ -7,107 +7,114 @@ import sampleFishes from '../sample-fishes';
 import base from '../base';
 
 class App extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.addFish = this.addFish.bind(this);
-        this.updateFish = this.updateFish.bind(this);
-        this.loadSamples = this.loadSamples.bind(this);
-        this.addToOrder = this.addToOrder.bind(this);
+    this.addFish = this.addFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
-        //get initial state
-        this.state = {
-            fishes: {},
-            order: {}
-        };
+    //get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  componentWillMount() {
+    // this runs right before the <App> is rendered
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+      , {
+        context: this,
+        state: 'fishes'
+      });
+
+    // check if there is any order in localStorage
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+    if (localStorageRef) {
+      //update our App component's order state
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
     }
+  }
 
-    componentWillMount() {
-        // this runs right before the <App> is rendered
-        this.ref = base.syncState(`${this.props.params.storeId}/fishes`
-            , {
-                context: this,
-                state: 'fishes'
-            });
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
-        // check if there is any order in localStorage
-        const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`,
+      JSON.stringify(nextState.order));
+  }
 
-        if (localStorageRef) {
-            //update our App component's order state
-            this.setState({
-                order: JSON.parse(localStorageRef)
-            })
-        }
-    }
+  addFish(fish) {
+    // update our state
+    const fishes = { ...this.state.fishes };
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes });
+  }
 
-    componentWillUnmount() {
-        base.removeBinding(this.ref);
-    }
+  updateFish(key, updatedFish) {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
 
-    componentWillUpdate(nextProps, nextState) {
-        localStorage.setItem(`order-${this.props.params.storeId}`,
-            JSON.stringify(nextState.order));
-    }
+  removeFish(key) {
+    const fishes = { ...this.state.fishes }
+    fishes[key] = null;
+    this.setState({ fishes });
+  }
 
-    addFish(fish) {
-        // update our state
-        const fishes = { ...this.state.fishes };
-        // add in our new fish
-        const timestamp = Date.now();
-        fishes[`fish-${timestamp}`] = fish;
-        // set state
-        this.setState({ fishes });
-    }
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
 
-    updateFish(key, updatedFish) {
-        const fishes = { ...this.state.fishes };
-        fishes[key] = updatedFish;
-        this.setState({ fishes });
-    }
+  addToOrder(key) {
+    // take a copy of our state
+    const order = { ...this.state.order };
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
+  }
 
-    loadSamples() {
-        this.setState({
-            fishes: sampleFishes
-        })
-    }
-
-    addToOrder(key) {
-        // take a copy of our state
-        const order = { ...this.state.order };
-        // update or add the new number of fish ordered
-        order[key] = order[key] + 1 || 1;
-        // update our state
-        this.setState({ order });
-    }
-
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market" />
-                    <ul className="list-of-fishes">
-                        {
-                            Object
-                                .keys(this.state.fishes)
-                                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
-                        }
-                    </ul>
-                </div>
-                <Order
-                    fishes={this.state.fishes}
-                    order={this.state.order}
-                    params={this.props.params}
-                />
-                <Inventory
-                    addFish={this.addFish}
-                    loadSamples={this.loadSamples}
-                    fishes={this.state.fishes}
-                    updateFish={this.updateFish}
-                />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
+            }
+          </ul>
+        </div>
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          params={this.props.params}
+        />
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
